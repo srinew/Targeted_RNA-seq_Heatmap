2ac9fc097bd1fb24e8d5a5fec3f7718ab393ddc7
Srinivas Nallandhighal <33036242+srinew@users.noreply.github.com>
2019-01-02 11:30:51 -0500

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,35 @@
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel(div(HTML("RNA-Seq Count Normalized Heatmap"))),
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file", "Choose CSV File",
+                accept = c(
+                  "text/csv",
+                  "text/comma-separated-values,text/plain",
+                  ".csv")
+      ),
+      # checkboxInput("header", "Header", TRUE), 
+      radioButtons("select_class",
+                         "Normalization method:",
+                         selected = c("Housekeeping normalization"),
+                         choiceNames = list("Housekeeping normalization",
+                                            "edgeR TMM normalization"),
+                         choiceValues = list("Housekeeping normalization",
+                                             "edgeR TMM normalization"),
+                         width = 2000),
+      radioButtons("select_class_1",
+                   "Sample Clustering:",
+                   selected = c("Yes"),
+                   choiceNames = list("Yes",
+                                      "No"),
+                   choiceValues = list("Yes",
+                                       "No"),
+                   width = 200), downloadButton("export", label = "download heatmap"),
+      width = 2),
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+)
